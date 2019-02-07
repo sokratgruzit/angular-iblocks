94f108702d965dd8e30708cec7799026236f4b0a
David Tavadze <tavadzed@gmail.com>
2019-02-08 00:17:26 +0400

    Made functions for flat data and flat modal

--- src/app/components/presentation/pr-facade/pr-facade.component.ts
+++ src/app/components/presentation/pr-facade/pr-facade.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LayoutFloorService } from '../../../services/layout-floor.service';
-import { tippy } from '../../../../assets/js/tippy.js';
+import { FlatsListService } from '../../../services/flats-list.service';
+import { AsideMenuComponent } from '../aside-menu/aside-menu.component';
+import { HttpClient } from '@angular/common/http';
 declare var $: any;
 
 @Component({
@@ -10,9 +12,10 @@ declare var $: any;
 })
 export class PrFacadeComponent implements OnInit {
   floorsList = [];
+  flatsCoordsList = [];
   flatsList = [];
 
-  constructor(public layoutFloorService: LayoutFloorService) { }
+  constructor(public layoutFloorService: LayoutFloorService, public flatsListService: FlatsListService) { }
 
   getFloors() {
     this.layoutFloorService.getAllFloorsData()
@@ -21,36 +24,38 @@ export class PrFacadeComponent implements OnInit {
     });
   }
 
-  getFlats(buildingId, floorId) {
+  getFlatsCoords(buildingId, floorId) {
     this.layoutFloorService.getAllFloorsData()
     .subscribe((data) => {
-      this.flatsList = data[0].properties.flats;
+      this.flatsCoordsList = data[0].properties.flats;
+    });
+  }
+
+  getFlatsList() {
+    this.flatsListService.getAllFlatsData()
+    .subscribe((data) => {
+      this.flatsList = data;
     });
   }
 
   ngOnInit() {
     $(function () {
-      $('.floor_modal_image').maphilight();
+      $('#floor_modal_image').maphilight();
       $('.map').maphilight();
       $('img[usemap]').rwdImageMaps();
       // $('img[usemap]').mapify();
-
-      // tippy.setDefaults({
-      //   arrow: true,
-      //   delay: 40,
-      //   theme: 'my-tippy',
-      // });
     });
 
     this.getFloors();
-    this.getFlats(1, 1);
+    this.getFlatsCoords(1, 1);
+    this.getFlatsList();
   }
 
   floorDetails(e, floorData, width, height) {
     const curAreaCoords = e.target.coords.split(',');
     let xMax = 0;
     let yMax = 0;
-    
+
     for (let i = 0; i < Math.trunc(curAreaCoords.length / 2); i++) {
       if (+curAreaCoords[2 * i] > xMax) {
         xMax = +curAreaCoords[2 * i];
@@ -59,7 +64,7 @@ export class PrFacadeComponent implements OnInit {
       if (+curAreaCoords[2 * i + 1] > yMax) {
         yMax = +curAreaCoords[2 * i + 1];
       }
-    };
+    }
 
     const posX = xMax;
     const posY = yMax;
@@ -86,36 +91,111 @@ export class PrFacadeComponent implements OnInit {
     $('.floor_data').empty();
   }
 
-  openModal(e, width, height) {
+  openModal(e, width, height, floorId) {
     const posX = e.clientX;
     const posY = e.clientY;
 
-    $('.floor_modal_image').attr('src', '../../../../assets/img/1_floor.png');
-    $('.floor_modal_image')
+    $('#floor_modal_image')
+    .attr('src', '../../../../assets/img/1_floor.png')
     .attr('style',
-    'width:' + width +
-    'px;height:' + height + 'px;');
-
+      'width:' + width +
+      'px;height:' + height + 'px;'
+    );
     $('#floor_modal')
     .attr('style',
       'width:' + width + 'px;height:' + height +
       'px;left:calc(50% - ' + width / 2 + 'px);'
     );
-
-    $('#floor_modal').toggleClass('zoom_in_floor zoom_out_floor');
+    $('#floor_modal').toggleClass('zoom_in_object zoom_out_object');
     $('.floor_modal_close').attr('style', 'display:block');
+
     $('.background_layer').fadeIn(350);
   }
 
   closeModal(e) {
-    $('#floor_modal').toggleClass('zoom_in_floor zoom_out_floor');
+    $('#floor_modal').toggleClass('zoom_in_object zoom_out_object');
     $('.floor_modal_close').attr('style', '');
     $('.background_layer').fadeOut(350);
+
+    this.closeAsideMenu();
+  }
+
+  openFlatModal(id, type) {
+    const curFlat = this.flatsList.filter(obj => {
+      return obj.id === id;
+    });
+
+    $('#floor_modal')
+    .attr('style',
+      'width:' + curFlat[0].imageWidth +
+      'px;height:' + curFlat[0].imageHeight +
+      'px;top:10%;left:calc(50% - ' + curFlat[0].imageWidth / 2 + 'px);'
+    )
+    .toggleClass('zoom_in_object zoom_out_object');
+
+    $('#flat_modal_image')
+    .attr('src', curFlat[0].layoutImgUrl)
+    .attr('style',
+      'width:' + curFlat[0].imageWidth +
+      'px;height:' + curFlat[0].imageHeight +
+      'px;display:block;'
+    );
+
+    this.openAsideMenu(id, type);
+  }
+
+  openAsideMenu(id, type) {
+    if (type === 'floor') {
+      $('#aside_menu').animate({
+        right: 0
+      }, 350);
+
+      for (let i = 0; i < this.flatsList.length; i++) {
+        const curFloor = this.flatsList[i];
+        const div = document.createElement('div');
+        const text = document.createTextNode('Apartament numger: ' + curFloor.apartmentNumber +
+        '; Floor number: ' + curFloor.floor +
+        '; Price: ' + curFloor.price + curFloor.currency + ';');
+
+        div.appendChild(text);
+        div.setAttribute('class', 'flats_list');
+        $('.aside_menu_content').append(div);
+      }
+    } else if (type === 'flat') {
+      const curFlat = this.flatsList.filter(obj => {
+        return obj.id === id;
+      });
+
+      $('.flats_list').remove();
+      $('.flat_data').remove();
+
+      const div = document.createElement('div');
+      const text = document.createTextNode(
+        'Apartament numger: ' + curFlat[0].apartmentNumber +
+        '; Floor number: ' + curFlat[0].floor +
+        '; Price: ' + curFlat[0].price + curFlat[0].currency + ';'
+      );
+
+      div.appendChild(text);
+      div.setAttribute('class', 'flat_data');
+
+      $('.aside_menu_content').append(div);
+
+    }
+
+  }
+
+  closeAsideMenu() {
+    $('#aside_menu').animate({
+      right: '-250px'
+    }, 350);
+
+    $('.flats_list').remove();
+    $('.flat_data').remove();
   }
 
   resizeReloader() {
     window.location.reload();
   }
-
 
 }
